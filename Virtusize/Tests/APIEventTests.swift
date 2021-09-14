fe4887d1cb55558d9f32ed1ed45c250611f050c7
Kuei <akueisara39@gmail.com>
2021-09-14 20:04:13 +0900

    Fix a test

--- Virtusize/Tests/APIEventTests.swift
+++ Virtusize/Tests/APIEventTests.swift
@@ -52,8 +52,8 @@ class APIEventTests: XCTestCase {
                        UIDevice.current.orientation.isLandscape ? "landscape" : "portrait"
         )
         XCTAssertEqual(payloadJson?["browserResolution"], "\(Int(screenSize.height))x\(Int(screenSize.width))")
-        XCTAssertEqual(payloadJson?["integrationVersion"], "2.3.1")
-        XCTAssertEqual(payloadJson?["snippetVersion"], "2.3.1")
+        XCTAssertEqual(payloadJson?["integrationVersion"], "2.3.2")
+        XCTAssertEqual(payloadJson?["snippetVersion"], "2.3.2")
     }
 
     func testAPIEvent_alignProductDataCheckContext_hasExpectedPayload() {
