ff4151a7331f9204944ff5aa5253fb5d27aa9ec0
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-10-02 09:16:39 +0900

    Format Package.swift

--- Package.swift
+++ Package.swift
@@ -2,38 +2,38 @@
 import PackageDescription
 
 let package = Package(
-	name: "Virtusize",
-	defaultLocalization: "en",
-	platforms: [.iOS(.v13)],
-	products: [
-		.library(
-			name: "Virtusize",
-			targets: ["Virtusize"]
+    name: "Virtusize",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "Virtusize",
+            targets: ["Virtusize"]
         ),
         .library(
             name: "VirtusizeCore",
             targets: ["VirtusizeCore"]
         )
-	],
-	dependencies: [],
-	targets: [
-		.target(
-			name: "Virtusize",
-			dependencies: ["VirtusizeCore", "VirtusizeAuth"],
-			path: "Virtusize/Sources",
-			exclude: ["Info.plist"],
-			resources: [.process("Resources")]
-		),
-		.target(
-			name: "VirtusizeCore",
-			dependencies: [],
-			path: "VirtusizeCore/Sources",
-			exclude: ["Info.plist"],
-			resources: [.process("Resources")]
-		),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Virtusize",
+            dependencies: ["VirtusizeCore", "VirtusizeAuth"],
+            path: "Virtusize/Sources",
+            exclude: ["Info.plist"],
+            resources: [.process("Resources")]
+        ),
+        .target(
+            name: "VirtusizeCore",
+            dependencies: [],
+            path: "VirtusizeCore/Sources",
+            exclude: ["Info.plist"],
+            resources: [.process("Resources")]
+        ),
         .binaryTarget(
             name: "VirtusizeAuth",
             path: "Frameworks/VirtusizeAuth.xcframework"
         )
-	]
+    ]
 )
