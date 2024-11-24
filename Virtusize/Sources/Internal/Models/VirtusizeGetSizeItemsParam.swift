1468d6da1a26fbeacf69616c7e1d62fa3a85f70a
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-11-24 11:15:18 +0900

    Refactor VirtusizeGetSizeItemsParam

--- Virtusize/Sources/Internal/Models/VirtusizeGetSizeItemsParam.swift
+++ Virtusize/Sources/Internal/Models/VirtusizeGetSizeItemsParam.swift
@@ -1,17 +1,29 @@
 //
-//  VirtusizeGetSizeItemsParama.swift
-//  Virtusize
+//  Copyright (c) 2023 Virtusize KK
 //
-//  Created by admin on 13/12/23.
-//  Copyright © 2023 Virtusize AB. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 struct VirtusizeGetSizeItemsParam: Codable {
-
     var additionalInfo: [String: VirtusizeAnyCodable] = [:]
     var itemSizesOrig: [String: [String: Int?]] = [:]
     var productType: String
     var extProductId: String
-
 }
