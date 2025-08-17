730c68db729cb4f9356b6cf711de63c775d067ed
Mikhail Yevstratiev <mikhail.yevstratiev@diversido.io>
2025-08-17 17:04:34 +0300

    Release 2.12.8

--- Virtusize/Tests/APIEventTests.swift
+++ Virtusize/Tests/APIEventTests.swift
@@ -52,8 +52,8 @@ class APIEventTests: XCTestCase {
                        UIDevice.current.orientation.isLandscape ? "landscape" : "portrait"
         )
         XCTAssertEqual(payloadJson?["browserResolution"], "\(Int(screenSize.height))x\(Int(screenSize.width))")
-        XCTAssertEqual(payloadJson?["integrationVersion"], "2.12.7")
-        XCTAssertEqual(payloadJson?["snippetVersion"], "2.12.7")
+        XCTAssertEqual(payloadJson?["integrationVersion"], "2.12.8")
+        XCTAssertEqual(payloadJson?["snippetVersion"], "2.12.8")
     }
 
     func testAPIEvent_alignProductCheckDataContext_hasExpectedPayload() {
