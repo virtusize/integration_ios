01eaa2cd2b38fb678011612d208ae373957c430b
OleS <oleg.semenyshyn@softblues-solutions.com>
2026-04-27 11:47:32 +0300

    Increment build version

--- Virtusize/Tests/APIEventTests.swift
+++ Virtusize/Tests/APIEventTests.swift
@@ -52,8 +52,8 @@ class APIEventTests: XCTestCase {
                        UIDevice.current.orientation.isLandscape ? "landscape" : "portrait"
         )
         XCTAssertEqual(payloadJson?["browserResolution"], "\(Int(screenSize.height))x\(Int(screenSize.width))")
-        XCTAssertEqual(payloadJson?["integrationVersion"], "2.12.28")
-        XCTAssertEqual(payloadJson?["snippetVersion"], "2.12.28")
+        XCTAssertEqual(payloadJson?["integrationVersion"], "2.12.29")
+        XCTAssertEqual(payloadJson?["snippetVersion"], "2.12.29")
     }
 
     func testAPIEvent_alignProductCheckDataContext_hasExpectedPayload() {
